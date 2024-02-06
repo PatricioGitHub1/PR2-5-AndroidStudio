b126c22c08fda183efcc6d121d2de34258a8f6c1
PatricioGitHub1 <rojas_patricio_github@gmx.es>
2024-02-06 18:31:32 +0100

    finishedgit add .

--- app/src/main/java/com/example/sensors_app/MainActivity.java
+++ app/src/main/java/com/example/sensors_app/MainActivity.java
@@ -8,7 +8,10 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,9 +19,32 @@ public class MainActivity extends AppCompatActivity {
     private Sensor sensor;
     SensorEventListener sensorListener;
 
-    @Override protected void onCreate(Bundle savedInstanceState) {
+    TextView textX;
+    TextView textZ;
+    TextView textY;
+    final Context context = this;
+    final GestureDetector.SimpleOnGestureListener listener = new GestureDetector.SimpleOnGestureListener() {
+        @Override
+        public boolean onDoubleTap(MotionEvent e) {
+            Toast.makeText(context, "onDoubleTap", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+
+        @Override
+        public void onLongPress(MotionEvent e) {
+            Toast.makeText(context, "onLongPress", Toast.LENGTH_SHORT).show();
+        }
+    };
+    GestureDetector detector;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textX = findViewById(R.id.textX);
+        textZ = findViewById(R.id.textZ);
+        textY = findViewById(R.id.textY);
 
         sensorListener = new SensorEventListener() {
             @Override
@@ -29,9 +55,13 @@ public class MainActivity extends AppCompatActivity {
                 float zAcc = sensorEvent.values[2];
 
                 // Processament o visualització de dades...
-                // Log.i("info", "Xacc" + String.valueOf(xAcc));
-                // Log.i("info", "Yacc" + String.valueOf(yAcc));
-                // Log.i("info", "Zacc" + String.valueOf(zAcc));
+                xAcc = (float) (Math.round(xAcc * 100.0) / 100.0);
+                yAcc = (float) (Math.round(yAcc * 100.0) / 100.0);
+                zAcc = (float) (Math.round(zAcc * 100.0) / 100.0);
+
+                textX.setText(String.valueOf(xAcc));
+                textZ.setText(String.valueOf(zAcc));
+                textY.setText(String.valueOf(yAcc));
             }
 
             @Override
@@ -40,15 +70,25 @@ public class MainActivity extends AppCompatActivity {
             }
         };
 
-        // Seleccionem el tipus de sensor (veure doc oficial)
+        detector = new GestureDetector(context, listener);
+
+        // listeners double tap
+        detector.setOnDoubleTapListener(listener);
+        detector.setIsLongpressEnabled(true);
+
+        // Select the type of sensor (see official documentation)
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 
-        // registrem el Listener per capturar els events del sensor
-        if( sensor!=null ) {
-            sensorManager.registerListener(sensorListener,sensor,
-                    SensorManager.SENSOR_DELAY_NORMAL);
+        // Register the listener to capture sensor events
+        if (sensor != null) {
+            sensorManager.registerListener(sensorListener, sensor, SensorManager.SENSOR_DELAY_NORMAL);
         }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return detector.onTouchEvent(event) || super.onTouchEvent(event);
     }
 }
 
